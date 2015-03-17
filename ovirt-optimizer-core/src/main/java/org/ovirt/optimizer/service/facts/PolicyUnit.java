8f0d4324d9877c22321510b49b9d7d488fbb0e31
Martin Sivak <msivak@redhat.com>
2015-03-17 12:33:30 +0100

    Fix PolicyUnit enum to use Strings for UUIDs (same as PolicyUnitEnabled)
    Change-Id: Icf975a7b8eb87f033cfa61752750a8a001a327d4
    Signed-off-by: Martin Sivak <msivak@redhat.com>

--- ovirt-optimizer-core/src/main/java/org/ovirt/optimizer/service/facts/PolicyUnit.java
+++ ovirt-optimizer-core/src/main/java/org/ovirt/optimizer/service/facts/PolicyUnit.java
@@ -7,17 +7,17 @@ public enum PolicyUnit {
     PIN_TO_HOST_FILTER("12262ab6-9690-4bc3-a2b3-35573b172d54"),
     MEMORY_FILTER("c9ddbb34-0e1d-4061-a8d7-b0893fa80932");
 
-    final UUID uuid;
+    final String uuid;
 
     PolicyUnit(UUID uuid) {
-        this.uuid = uuid;
+        this.uuid = uuid.toString();
     }
 
     PolicyUnit(String uuid) {
-        this.uuid = UUID.fromString(uuid);
+        this.uuid = uuid;
     }
 
-    public UUID getUuid() {
+    public String getUuid() {
         return uuid;
     }
 }
