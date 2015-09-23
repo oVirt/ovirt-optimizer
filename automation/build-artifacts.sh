19a41d9292bf073ef411af8626a6966f1c4fb7a3
Sandro Bonazzola <sbonazzo@redhat.com>
2015-09-23 15:35:06 +0200

    automation: add standard build automation
    Change-Id: I7520c4789a356e77372c9a51fef9f8622ec7b92a
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ automation/build-artifacts.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+[[ -d rpmbuild/SOURCES ]] \
+|| mkdir -p rpmbuild/SOURCES
+
+MAVEN_SETTINGS="/etc/maven/settings.xml"
+
+# Use ovirt mirror if able, fall back to central maven
+cat >"$MAVEN_SETTINGS" <<EOS
+<?xml version="1.0"?>
+<settings xmlns="http://maven.apache.org/POM/4.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
+          http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+<mirrors>
+        <mirror>
+                <id>ovirt-maven-repository</id>
+                <name>oVirt artifactory proxy</name>
+                <url>http://artifactory.ovirt.org:8081/artifactory/ovirt-mirror</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+        <mirror>
+                <id>root-maven-repository</id>
+                <name>Official maven repo</name>
+                <url>http://repo.maven.apache.org/maven2</url>
+                <mirrorOf>*</mirrorOf>
+        </mirror>
+</mirrors>
+</settings>
+EOS
+
+
+# Build RPMs
+sed -i -e 's/mvn --offline /mvn /g' ovirt-optimizer.spec
+mvn help:evaluate -Dexpression=project.version -gs "$MAVEN_SETTINGS" # downloads and installs the necessary jars
+
+# Prepare the version string (with support for SNAPSHOT versioning)
+VERSION=$(mvn help:evaluate -Dexpression=project.version -gs "$MAVEN_SETTINGS" 2>/dev/null| grep -v "^\[")
+VERSION=${VERSION/-SNAPSHOT/-0.$(git rev-list HEAD | wc -l).$(date +%04Y%02m%02d%02H%02M)}
+IFS='-' read -ra VERSION <<< "$VERSION"
+
+git archive --format=tar HEAD | gzip -9 >$HOME/rpmbuild/SOURCES/ovirt-optimizer-$VERSION.tar.gz
+
+echo `rpm --eval "Fedora:0%{fedora} ; RHEL: 0%{rhel}"`
+
+rpmbuild \
+    --define "_topmdir $PWD/rpmbuild" \
+    --define "_rpmdir $PWD/rpmbuild" \
+    --define "_version $VERSION" \
+    --define "_release ${VERSION[1]-1}" \
+    --define "with_extra_maven_opts -gs $MAVEN_SETTINGS" \
+    -ba --nodeps ovirt-optimizer.spec
+
+# Move RPMs to exported artifacts
+find rpmbuild -iname \*rpm -exec mv "{}" exported-artifacts/ \;
